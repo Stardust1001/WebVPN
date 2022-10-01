684008fdd49c9a997b20b1e0864ebae403fadb66
Stardust <lqqliebe@foxmail.com>
2022-10-01 22:59:25 +0800

    add

--- main.js
+++ main.js
@@ -72,19 +72,15 @@ class VPN extends WebVPN {
 // 下面都可以改，这是我自己做的示例
 // 最主要的是继承并实现 WebVPN 类，然后提供一些配置 config
 
-const prodSite = 'http://www.webvpn-proxy.com/proxy'
-const devSite = 'http://127.0.0.1:1001/proxy'
-const isProd = os.type() !== 'Darwin'
-
 const config = {
 	// WebVPN 服务端口
-	port: 1001,
+	port: 80,
 	// WebVPN 服务网址，访问其他网站，都从这个网址进行转换
-	site: new URL(isProd && prodSite || devSite),
+	site: new URL('http://www.webvpn.com'),
 	// cluster 模式用几个进程（为了充分利用CPU核心数）
 	numProcesses: 4,
 	// 是否启用缓存，会把静态资源缓存到本地文件夹以加速后续的网站访问
-	cache: isProd,
+	cache: true,
 	// 缓存文件夹地址
 	cacheDir: 'cache',
 	// 是否在浏览器控制台打印拦截操作的日志
@@ -105,7 +101,7 @@ const vpn = new VPN(config)
 // 启动 WebVPN 服务
 vpn.start()
 
-if (!isProd && cluster.isMaster) {
+if (cluster.isMaster) {
 	setTimeout(() => {
 		// 运行后自动打开 WebVPN 网址
 		// childProcess.exec('open ' + new URL(devSite).origin)
