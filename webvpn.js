fcf1d2c03f01d43721ea4b5a9e83e1d612e0c4cb
Stardust1001 <2368354416@qq.com>
2022-10-18 16:36:29 +0800

    大括号检测更新修复

--- webvpn.js
+++ webvpn.js
@@ -535,9 +535,19 @@ class WebVPN {
 		let current = ''
 		let last = ''
 		for (let i = 0; i < len; i++) {
-			if (regexpRanges.some(r => r[0] <= i && r[1] > i)) {
-				last = code[i]
-				continue
+			// 虽然这样做可能也会错，先这样
+			// 这是为了避免错误的正则匹配影响，以这里循环检测的为主，如果当前是字符串或注释，那么上面匹配到这里的正则表达式视为无效
+			// 否则就跳过这里的正则表达式区域
+			const rangeIndex = regexpRanges.findIndex(r => r[0] <= i && r[1] > i)
+			if (rangeIndex >= 0) {
+				if (isStr || isComment) {
+					regexpMatches.splice(rangeIndex, 1)
+					regexpRanges.splice(rangeIndex, 1)
+				} else {
+					i = regexpRanges[rangeIndex][1] - 1
+					last = code[i]
+					continue
+				}
 			}
 			current = code[i]
 			if (isStr) {
@@ -567,18 +577,21 @@ class WebVPN {
 				last = current
 				continue
 			}
-			if (last === '/') {
+			// 发现注释就直接跳到注释的结尾吧，不循环无意义的注释
+			if (last === '/' && code[i - 2] !== '\\') {
 				if (current === '/') {
 					isComment = true
 					isSingleComment = true
 					isStr = false
-					last = current
+					i = i + code.slice(i + 1).indexOf('\n')
+					last = code[i]
 					continue
 				} else if (current === '*') {
 					isComment = true
 					isSingleComment = false
 					isStr = false
-					last = current
+					i = i + code.slice(i + 1).indexOf('*/')
+					last = code[i]
 					continue
 				}
 			}
